1631ffab92ebaf920213a0754511f660adffe24e
kwakdo <vv05044@gmail.com>
2022-05-16 18:21:21 +0900

    prac

--- sql-practices/연습문제/practice03.sql
+++ sql-practices/연습문제/practice03.sql
@@ -11,27 +11,93 @@ order by b.salary desc;
 
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
+select a.emp_no as '사번', a.first_name as '이름', b.title as '직책'
+  from employees a, titles b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+group by a.first_name
+order by a.first_name;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요..
+select a.emp_no as '사번', a.first_name as '이름', b.title as '직책'
+  from employees a, titles b, dept_emp c, departments d
+ where a.emp_no = b.emp_no
+   and b.emp_no = c.emp_no
+   and c.dept_no = d.dept_no
+   and b.to_date = '9999-01-01'
+   and c.to_date = '9999-01-01'
+group by a.first_name
+order by d.dept_name;
 
 -- 문제4.
 -- 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
+select a.emp_no as '사번', a.first_name as '이름', e.salary as '연봉', b.title as '직책', d.dept_name as '부서'
+  from employees a, titles b, dept_emp c, departments d, salaries e
+ where a.emp_no = b.emp_no
+   and b.emp_no = c.emp_no
+   and c.dept_no = d.dept_no
+   and a.emp_no = e.emp_no
+   and b.to_date = '9999-01-01'
+   and c.to_date = '9999-01-01'
+group by a.first_name
+order by a.first_name;
 
 -- 문제5.
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. (현재 ‘Technique Leader’의 직책(으로 근무하는 사원은 고려하지 않습니다.) 이름은 first_name과 last_name을 합쳐 출력 합니다.
+select a.emp_no as '사번', concat(a.first_name, ' ', a.last_name) as '이름', b.title as '직책'
+  from employees a, titles b
+ where a.emp_no = b.emp_no
+   and b.title = 'Technique Leader'
+   and b.to_date != '9999-01-01'
+   group by a.first_name;
 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
+select concat(first_name, ' ', last_name) as '이름',c.dept_name as '부서명', b.title as '직책'
+  from employees a, titles b, departments c, dept_emp d
+ where a.emp_no = b.emp_no
+   and b.emp_no = d.emp_no
+   and c.dept_no = d.dept_no
+   and a.last_name like 'S%'
+   group by a.first_name;
 
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
+select a.first_name as '이름', c.salary as '급여'
+  from employees a, titles b, salaries c
+ where a.emp_no = b.emp_no
+   and b.emp_no = c.emp_no
+   and b.to_date = '9999-01-01'
+   and c.to_date = '9999-01-01'
+   and salary >= 40000
+group by a.first_name
+order by c.salary desc;
 
 -- 문제8.
 -- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
+select a.title as '직책', b.salary '급여'
+from titles a, salaries b
+where a.emp_no = b.emp_no
+and b.to_date = '9999-01-01'
+and salary >= 50000
+order by b.salary desc;
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
+select a.dept_no as '부서번호', avg(salary) as '평균연봉'
+    from dept_emp a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.dept_no;
+ 
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
+select a.title, max(salary)
+    from titles a, salaries b
+   where a.emp_no = b.emp_no
+     and a.to_date = '9999-01-01'
+     and b.to_date = '9999-01-01'
+group by a.title;
