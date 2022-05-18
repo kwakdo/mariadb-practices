4c47671d20fef50a0f843575aa5e7666b01a5909
kwakdo <vv05044@gmail.com>
2022-05-18 17:49:55 +0900

    HR Main

---
+++ jdbc-practices/src/main/java/hr/main/HRMain02.java
@@ -0,0 +1,26 @@
+package hr.main;
+
+import java.util.List;
+import java.util.Scanner;
+
+import hr.dao.EmployeeDao;
+import hr.vo.EmployeeVo;
+
+public class HRMain02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("salary[min max]>");
+		int minSalary = scanner.nextInt();
+		int maxSalary = scanner.nextInt();
+		
+		List<EmployeeVo> list = new EmployeeDao().findBySalary(minSalary, maxSalary);
+		
+				// no:firstName:lastName:salary
+				// order 큰순
+		
+		scanner.close();
+	}
+
+}
