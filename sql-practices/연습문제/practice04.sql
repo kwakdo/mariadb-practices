ae16fe0e5d22880c8acd097dbc1018442eee799a
kwakdo <vv05044@gmail.com>
2022-05-18 09:36:00 +0900

    done

--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -1,12 +1,18 @@
 -- 서브쿼리(SUBQUERY) SQL 문제입니다.
 -- 문제1.
 -- 현재 평균 연봉보다 많은 월급을 받는 직원은 몇 명이나 있습니까?
-select count(emp_no)
-  from salaries
- where to_date = '9999-01-01'
-   and salary < (select avg(salary)
-                         from  salaries
-                        where to_date = '9999-01-01');
+SELECT 
+    COUNT(emp_no)
+FROM
+    salaries
+WHERE
+    to_date = '9999-01-01'
+        AND salary < (SELECT 
+            AVG(salary)
+        FROM
+            salaries
+        WHERE
+            to_date = '9999-01-01');
 
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
@@ -53,30 +59,36 @@ group by a.first_name;
    
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
-select a.emp_no, a.first_name, b.title, c.salary , d.dept_no
-from employees a, titles b, salaries c, dept_emp d
-where a.emp_no = b.emp_no
-and a.emp_no= c.emp_no
-and a.emp_no=d.emp_no
-and b.to_date='9999-01-01'
-and c.to_date='9999-01-01'
-and d.dept_no =(
-	select dept_no
-	from(select b.dept_no, avg(a.salary) as avg_salary
-	from salaries a, dept_emp b
-	where a.emp_no = b.emp_no
-	and a.to_date='9999-01-01'
-	and b.to_date='9999-01-01'
-	group by b.dept_no) e
-	where y.avg_salary = (
-	select max(avg_salary)
-	from(select b.dept_no, avg(a.salary) as avg_salary
-	from salaries a, dept_emp b
-	where a.emp_no = b.emp_no
-	and b.to_date='9999-01-01'
-	and a.to_date='9999-01-01'
-	group by b.dept_no f))
-order by c.salary desc;
+SELECT 
+    a.emp_no,
+    CONCAT(a.first_name, a.last_name) name,
+    c.title,
+    d.salary
+FROM
+    employees a,
+    dept_emp b,
+    titles c,
+    salaries d
+WHERE
+    a.emp_no = b.emp_no
+        AND a.emp_no = c.emp_no
+        AND a.emp_no = d.emp_no
+        AND b.dept_no = (SELECT 
+            s.dept_no
+        FROM
+            (SELECT 
+                AVG(a.salary) AS salary, b.dept_no
+            FROM
+                salaries a, dept_emp b
+            WHERE
+                a.emp_no = b.emp_no
+            GROUP BY b.dept_no) s
+        ORDER BY s.salary DESC
+        LIMIT 1)
+        AND b.to_date = '9999-01-01'
+        AND c.to_date = '9999-01-01'
+        AND d.to_date = '9999-01-01'
+ORDER BY salary desc;
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
@@ -92,19 +104,31 @@ from departments a,
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
-select a.title, avg(a.salary), salary
-from titles a, salaries b
-where a.emp_no = b.emp_no
-group by a.title
-order by AVG(salary) desc
-limit 1;
+SELECT 
+    a.title, AVG(a.salary), salary
+FROM
+    titles a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+GROUP BY a.title
+ORDER BY AVG(salary) DESC
+LIMIT 1;
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
--- select d.dept_name as '부서이름', concat(a.fitst_name, ' ', a.last_name) as '사원이름', b.salary as '연봉', concat(e.fitst_name, ' ', e.last_name) as '매니저 이름', f.salary as '매니저연봉'
-<<<<<<< HEAD
--- from employees a, salaries b, dept_emp c, departments d, employees e, salaries f
-=======
--- from employees a, salaries b, dept_emp c, departments d, employees e, salaries f
->>>>>>> refs/remotes/origin/master
+select g.dept_name as '부서이름', concat(a.first_name, ' ', a.last_name) as '이름',max(e.salary) as '연봉', concat(d.first_name, ' ', d.last_name) as '매니저이름', f.salary as '매니저연봉'
+  from employees a, dept_emp b, dept_manager c, employees d, salaries e, salaries f, departments g
+ where a.emp_no = b.emp_no
+   and b.dept_no = c.dept_no
+   and c.emp_no = d.emp_no
+   and e.emp_no = a.emp_no
+   and f.emp_no = d.emp_no
+   and g.dept_no = b.dept_no
+   and f.to_date = '9999-01-01'
+   and e.to_date = '9999-01-01'
+   and c.to_date = '9999-01-01'
+   and b.to_date = '9999-01-01'
+group by g.dept_name, a.first_name, d.first_name, f.salary;
+
